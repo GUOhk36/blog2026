80d3aca7dd950e2bb6cbd38f56363919e37bd690
GUO23 <128574267+GUOhk36@users.noreply.github.com>
2026-02-14 22:28:41 +0800

    Update blog.config.ts

--- src/blog.config.ts
+++ src/blog.config.ts
@@ -7,10 +7,10 @@ export const blogConfig = {
   name: env.VITE_BLOG_NAME || "blog",
   author: env.VITE_BLOG_AUTHOR || "作者",
   description:
-    env.VITE_BLOG_DESCRIPTION || "这是博客的描述，写一段话介绍一下这个博客，",
+    env.VITE_BLOG_DESCRIPTION || "小满胜完全，人生只言片语碎片呓语，尘埃记录时代",
   social: {
     github: env.VITE_BLOG_GITHUB || "https://github.com/example",
-    email: env.VITE_BLOG_EMAIL || "demo@example.com",
+    email: env.VITE_BLOG_EMAIL || "abertaglar@proton.com",
   },
 };
 

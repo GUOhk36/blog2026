9111c84e7defba277cfaf709939094e4e3019469
GUO23 <128574267+GUOhk36@users.noreply.github.com>
2026-02-15 09:19:07 +0800

    Update blog.config.ts

--- src/blog.config.ts
+++ src/blog.config.ts
@@ -3,11 +3,11 @@ import { clientEnv } from "@/lib/env/client.env";
 const env = clientEnv();
 
 export const blogConfig = {
-  title: env.VITE_BLOG_TITLE || "Flare Stack Blog",
-  name: env.VITE_BLOG_NAME || "blog",
+  title: env.VITE_BLOG_TITLE || "Clark_Blog",
+  name: env.VITE_BLOG_NAME || "MenoFragments",
   author: env.VITE_BLOG_AUTHOR || "作者",
   description:
-    env.VITE_BLOG_DESCRIPTION || "小满胜万全，人生只言片语碎片呓语，尘埃记录时代",
+    env.VITE_BLOG_DESCRIPTION || "小满胜万全，人生只言片语碎片呓语，尘埃记录时代。",
   social: {
     github: env.VITE_BLOG_GITHUB || "https://github.com/example",
     email: env.VITE_BLOG_EMAIL || "abertaglar@proton.com",

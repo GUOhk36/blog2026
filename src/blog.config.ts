b996681fd5ff78a9cdb7b1a4d4d8ae2c342bb142
GUO23 <128574267+GUOhk36@users.noreply.github.com>
2026-02-15 10:06:04 +0800

    Update blog.config.ts

--- src/blog.config.ts
+++ src/blog.config.ts
@@ -3,8 +3,8 @@ import { clientEnv } from "@/lib/env/client.env";
 const env = clientEnv();
 
 export const blogConfig = {
-  title: env.VITE_BLOG_TITLE || "Clark_Blog",
-  name: env.VITE_BLOG_NAME || "MenoFragments",
+  title: env.VITE_BLOG_TITLE || "Clog",
+  name: env.VITE_BLOG_NAME || "MemoFragments",
   author: env.VITE_BLOG_AUTHOR || "作者",
   description:
     env.VITE_BLOG_DESCRIPTION || "小满胜万全，人生只言片语碎片呓语，尘埃记录时代。",
